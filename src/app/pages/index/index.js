9c29fd04a8f168c152cee1ae55d460fc267f49dc
Kamas Lau <kamaslau@outlook.com>
2021-10-27 12:58:01 +0800

    Changed 初步置入Vue

--- src/app/pages/index/index.js
+++ src/app/pages/index/index.js
@@ -1,3 +1,4 @@
+const Vue = require('vue')
 /**
  * 首页
  * ===
@@ -68,49 +69,6 @@ const pickPath = async () => {
   return path
 }
 
-/**
- * TODO 打包
- *
- * 直接创建压缩包到目标路径，不处理文件
- */
-const doPack = async () => {
-  console.log('doPack: ')
-
-  if (!sourcePath || !sourcePath) alert('需要指定源&目标路径')
-
-  await packPath(sourcePath, targetPath)
-}
-
-/**
- * 克隆
- *
- * 直接创建文件拷贝到目标路径，不处理文件
- */
-const doClone = async () => {
-  console.log('doClone: ')
-
-  if (!sourcePath || !sourcePath) alert('需要指定源&目标路径')
-
-  await clonePath(sourcePath, targetPath)
-}
-
-/**
- * 生成
- *
- * 使用源文件作为模板来生成新文件
- */
-const doGenerate = async () => {
-  console.log('doClone: ')
-
-  if (!sourcePath || !sourcePath) alert('需要指定源&目标路径')
-
-  Object.keys(biz).forEach(name => { biz[name] = getInput(name) })
-
-  await clonePath(sourcePath, targetPath, biz)
-  // const result = await clonePath(sourcePath, targetPath)
-  // console.log(result)
-}
-
 // 更新字段值
 const setInput = (name, value = '') => {
   document.querySelector(`input[name='${name}']`).value = value
@@ -130,15 +88,15 @@ const renderContent = () => {
 
 // 绑定事件监听器
 const bindEventListeners = () => {
-  document.getElementById('sample-source-path').innerText = appPathDict.documents
-  document.getElementById('target-source-path').innerText = appPathDict.desktop
+  // document.getElementById('sample-source-path').innerText = appPathDict.documents
+  // document.getElementById('target-source-path').innerText = appPathDict.desktop
 
-  document.getElementById('pick-source').addEventListener('click', async (event) => await pickSource())
-  document.getElementById('pick-target').addEventListener('click', async (event) => await pickTarget())
+  // document.getElementById('pick-source').addEventListener('click', async (event) => await pickSource())
+  // document.getElementById('pick-target').addEventListener('click', async (event) => await pickTarget())
 
-  document.getElementById('do-pack').addEventListener('click', async (event) => await doPack())
-  document.getElementById('do-clone').addEventListener('click', async (event) => await doClone())
-  document.getElementById('do-generate').addEventListener('click', async (event) => await doGenerate())
+  // document.getElementById('do-pack').addEventListener('click', async (event) => await doPack())
+  // document.getElementById('do-clone').addEventListener('click', async (event) => await doClone())
+  // document.getElementById('do-generate').addEventListener('click', async (event) => await doGenerate())
 }
 
 window.onload = () => {
@@ -149,5 +107,101 @@ window.onload = () => {
 
   renderContent()
 
-  bindEventListeners()
+  // bindEventListeners()
 }
+
+/**
+ * Vue 单页应用
+ */
+const App = {
+  data () {
+    return {
+      // 路径
+      sourcePath: isDev ? appPathDict[process.env.SOURCE_DIR] : '',
+      sourcePathSample: appPathDict[process.env.SOURCE_DIR],
+      targetPath: isDev ? appPathDict[process.env.TARGET_DIR] : '',
+      targetPathSample: appPathDict[process.env.TARGET_DIR],
+
+      // 业务
+      bizItem: {
+        code: '',
+        name: '',
+        nameLocale: ''
+      },
+      bizs: [],
+
+      // 数据库
+      db: {
+        url: 'mysql://root:123456@localhost:3306/xyz',
+        db: '',
+        table: '',
+        pk: ''
+      }
+    }
+  },
+
+  methods: {
+    // 选择源路径
+    pickSource: async () => {
+      console.log('pickSource: ')
+
+      sourcePath = await pickPath()
+
+      if (sourcePath !== null) setInput('source-path', sourcePath) // 更新字段值
+    },
+
+    // 选择目标路径
+    pickTarget: async () => {
+      console.log('pickTarget: ')
+
+      targetPath = await pickPath()
+
+      if (targetPath !== null) document.querySelector("input[name='target-path']").value = targetPath // 更新字段值
+    },
+
+    /**
+     * 打包
+     *
+     * 直接创建压缩包到目标路径，不处理文件
+     */
+    doPack: async () => {
+      console.log('doPack: ')
+
+      if (!sourcePath || !sourcePath) window.alert('需要指定源&目标路径')
+
+      await packPath(sourcePath, targetPath)
+    },
+
+    /**
+     * 克隆
+     *
+     * 直接创建文件拷贝到目标路径，不处理文件
+     */
+    doClone: async () => {
+      console.log('doClone: ')
+
+      if (!sourcePath || !sourcePath) window.alert('需要指定源&目标路径')
+
+      await clonePath(sourcePath, targetPath)
+    },
+
+    /**
+     * 生成
+     *
+     * 使用源文件作为模板来生成新文件
+     */
+    doGenerate: async () => {
+      console.log('doClone: ')
+
+      if (!sourcePath || !sourcePath) window.alert('需要指定源&目标路径')
+
+      Object.keys(biz).forEach(name => { biz[name] = getInput(name) })
+
+      await clonePath(sourcePath, targetPath, biz)
+    // const result = await clonePath(sourcePath, targetPath)
+    // console.log(result)
+    }
+  },
+
+}
+Vue.createApp(App).mount('#app')
