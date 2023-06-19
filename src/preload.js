6dad187bc2d70b5229f1f393ad395bf95229348b
Kamas "Iceberg" Lau <kamaslau@outlook.com>
2023-06-19 08:31:09 +0800

    feat: output env version in console

--- src/preload.js
+++ src/preload.js
@@ -2,13 +2,16 @@
 
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
   const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
+    const element = document.getElementById(selector);
+    if (element) element.innerText = text;
+  };
 
-  for (const dependency of ['chrome', 'node', 'electron']) {
-    replaceText(`${dependency}-version`, process.versions[dependency])
+  console.group("versions: ");
+  for (const dependency of ["chrome", "node", "electron"]) {
+    console.log(dependency.padStart(8), process.versions[dependency]);
+    // replaceText(`${dependency}-version`, process.versions[dependency]);
   }
-})
+  console.groupEnd();
+});
--- src/services/index.js
+++ src/services/index.js
@@ -1,17 +1,17 @@
 /**
  * IoC
  */
-const { clipboard, shell, ipcRenderer } = require('electron')
-const fetch = require('node-fetch')
-const fs = require('fs-extra')
-const JSZip = require('jszip') // https://stuk.github.io/jszip/documentation/examples.htmls
-const os = require('os')
-const path = require('path')
-const remote = require('@electron/remote')
-const Historit = require('../../utils/historit')
+const { clipboard, shell, ipcRenderer } = require("electron");
+const fetch = require("node-fetch");
+const fs = require("fs-extra");
+const JSZip = require("jszip"); // https://stuk.github.io/jszip/documentation/examples.htmls
+const os = require("node:os");
+const path = require("node:path");
+const remote = require("@electron/remote");
+const Historit = require("../../utils/historit");
 
-const isDev = process.env.NODE_ENV === 'development'
-console.log('isDev: ', isDev)
+const isDev = process.env.NODE_ENV === "development";
+console.log("isDev: ", isDev);
 
 /**
  * 应用路径词典
@@ -19,30 +19,30 @@ console.log('isDev: ', isDev)
  * https://www.electronjs.org/docs/latest/api/app#appgetpathname
  */
 const appPathDict = {
-  config: remote.app.getPath('userData'), // 本地安装路径
-  data: path.join(remote.app.getPath('appData'), 'com.kamaslau.AppCoder'), // 本地配置路径
-  desktop: remote.app.getPath('desktop'), // 桌面文件夹路径
-  documents: remote.app.getPath('documents'), // 用户文档文件夹路径
-  downloads: remote.app.getPath('downloads'), // 下载文件夹路径
-  recent: os.platform() === 'win32' ? remote.app.getPath('recent') : null, // 【仅Windows】“最近”文件夹路径
-  root: path.join(__dirname, '../../'), // 根路径；即app.js所在路径
-  libs: path.join(__dirname, '../../libs'), // 第三方库
-  page: path.join(__dirname, '../../page'), // 页面；重构时可以此作为前端框架的组件目录
-  static: path.join(__dirname, '../../static') // 静态资源（图片、样式、字体、模板文件等）
-}
+  config: remote.app.getPath("userData"), // 本地安装路径
+  data: path.join(remote.app.getPath("appData"), "com.kamaslau.AppCoder"), // 本地配置路径
+  desktop: remote.app.getPath("desktop"), // 桌面文件夹路径
+  documents: remote.app.getPath("documents"), // 用户文档文件夹路径
+  downloads: remote.app.getPath("downloads"), // 下载文件夹路径
+  recent: os.platform() === "win32" ? remote.app.getPath("recent") : null, // 【仅Windows】“最近”文件夹路径
+  root: path.join(__dirname, "../../"), // 根路径；即app.js所在路径
+  libs: path.join(__dirname, "../../libs"), // 第三方库
+  page: path.join(__dirname, "../../page"), // 页面；重构时可以此作为前端框架的组件目录
+  static: path.join(__dirname, "../../static"), // 静态资源（图片、样式、字体、模板文件等）
+};
 // console.log('appPathDict: ', appPathDict)
 
 // 应用主配置文件
-const appFile = path.join(appPathDict.data, 'config.json') // 生成配置文件
+const appFile = path.join(appPathDict.data, "config.json"); // 生成配置文件
 
 // 检查文件是否存在，若否则迭代创建文件（含路径）
 const touchFile = (filePath) => {
   try {
-    fs.ensureFileSync(filePath)
+    fs.ensureFileSync(filePath);
   } catch (error) {
-    console.error('touchConfig error: ', error)
+    console.error("touchConfig error: ", error);
   }
-}
+};
 
 /**
  * 替换内容中的占位字符串为实际值
@@ -54,16 +54,16 @@ const touchFile = (filePath) => {
 const replaceMatchedString = (context, searchValue, replaceValue) => {
   // console.log('replaceMatchedString: ', context, searchValue, replaceValue)
 
-  let result = ''
+  let result = "";
 
   try {
-    result = context.replaceAll(searchValue, replaceValue)
+    result = context.replaceAll(searchValue, replaceValue);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 
-  return result
-}
+  return result;
+};
 
 /**
  * 读取目标文件夹下文件列表
@@ -74,10 +74,10 @@ const replaceMatchedString = (context, searchValue, replaceValue) => {
 const listFilesInDir = async (targetDir = null) => {
   // console.log('listFilesInDir: ', targetDir)
 
-  if (targetDir === null) return
+  if (targetDir === null) return;
 
   // Fetch temp demo list
-  let result = []
+  let result = [];
   try {
     result = await fs
       .readdir(targetDir)
@@ -88,14 +88,14 @@ const listFilesInDir = async (targetDir = null) => {
       .then(
         // cancat path root to sole file names
         (list) => list.map((item) => path.join(targetDir, item))
-      )
+      );
   } catch (error) {
-    console.error('listFilesInDir error: ', error)
+    console.error("listFilesInDir error: ", error);
   }
   // console.log('result: ', result)
 
-  return result
-}
+  return result;
+};
 
 // 遍历并处理路径
 const processPath = async (
@@ -106,42 +106,42 @@ const processPath = async (
   // console.log('processPath: ', rootPath, typeof fileOp, typeof dirOp)
 
   // 获取当前路径下的所有文件
-  const paths = await listFilesInDir(rootPath) ?? []
+  const paths = (await listFilesInDir(rootPath)) ?? [];
 
-  if (paths.length === 0) return
+  if (paths.length === 0) return;
 
   try {
     for (const path of paths) {
-      const pathState = fs.lstatSync(path)
+      const pathState = fs.lstatSync(path);
 
       if (pathState.isDirectory()) {
         // 继续遍历目录
         // console.log(`${path} is a directory`)
 
-        if (typeof dirOp === 'function') await dirOp(path)
+        if (typeof dirOp === "function") await dirOp(path);
 
-        await processPath(path, fileOp, dirOp) // 迭代子目录
+        await processPath(path, fileOp, dirOp); // 迭代子目录
       } else if (pathState.isFile()) {
         // 处理文件
         // console.log(`${path} is a file`)
 
-        if (typeof fileOp === 'function') await fileOp(path)
+        if (typeof fileOp === "function") await fileOp(path);
       }
     }
   } catch (error) {
-    console.error('processPath error: ', error)
+    console.error("processPath error: ", error);
   }
-}
+};
 
 /**
  * 打包路径
  */
 const packPath = async (sourcePath, targetPath) => {
-  console.log(`packPath: ${sourcePath} -> ${targetPath}`)
+  console.log(`packPath: ${sourcePath} -> ${targetPath}`);
 
-  if (!sourcePath || !sourcePath) return
+  if (!sourcePath || !sourcePath) return;
 
-  const zip = new JSZip()
+  const zip = new JSZip();
   // zip.file('ziptest.txt', 'zip content') // DEMO only
 
   // 文件操作
@@ -149,45 +149,45 @@ const packPath = async (sourcePath, targetPath) => {
     // console.log('create zip file item')
 
     // 将待克隆文件相对于目标目录的路径增量部分，作为目标路径的一部分，以保持文件目录结构
-    const relativePath = filePath.substring(sourcePath.length).substring(1) // 移除开头的冗余"/"字符
+    const relativePath = filePath.substring(sourcePath.length).substring(1); // 移除开头的冗余"/"字符
 
     // 读取当前文件内容
-    const pageContent = await fs.readFile(filePath, 'utf8')
+    const pageContent = await fs.readFile(filePath, "utf8");
 
     // 将文件内容添加到zip实例
-    zip.file(relativePath, pageContent)
-    console.log('zip is now: ', zip)
-  }
+    zip.file(relativePath, pageContent);
+    console.log("zip is now: ", zip);
+  };
 
   // 迭代处理根目录下的路径
-  await processPath(sourcePath, fileOp)
+  await processPath(sourcePath, fileOp);
 
   // 在内存中生成zip文件
   const zipFileContent = await zip.generateAsync({
-    type: 'nodebuffer',
-    compression: 'DEFLATE',
-    compressionOptions: { level: 9 }
-  })
+    type: "nodebuffer",
+    compression: "DEFLATE",
+    compressionOptions: { level: 9 },
+  });
   // console.log(zip.files)
 
   // 写入新文件到目标路径
   const targetFilePath = `${targetPath}/${path.basename(
     sourcePath
-  )}_clone_packed.zip`
+  )}_clone_packed.zip`;
 
   try {
     fs.ensureFile(targetFilePath)
       .then(() => {
-        fs.writeFile(targetFilePath, zipFileContent)
+        fs.writeFile(targetFilePath, zipFileContent);
       })
       .then(() => {
-        console.log('zip file path is targetFilePath: ', targetFilePath)
-        shell.openPath(targetPath) // 在文件管理器中打开目标路径
-      })
+        console.log("zip file path is targetFilePath: ", targetFilePath);
+        shell.openPath(targetPath); // 在文件管理器中打开目标路径
+      });
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-}
+};
 
 /**
  * 克隆路径
@@ -196,20 +196,20 @@ const clonePath = async (
   sourcePath = null,
   targetPath = null,
   renameFile = true,
-  folderName = '',
+  folderName = "",
   payload = null
 ) => {
-  console.log(`clonePath: ${sourcePath} -> ${targetPath}`, payload)
+  console.log(`clonePath: ${sourcePath} -> ${targetPath}`, payload);
 
-  if (!sourcePath || !sourcePath) return
+  if (!sourcePath || !sourcePath) return;
 
   // 文件操作
   const fileOp = async (filePath) => {
-    const fileExtname = path.extname(filePath)
+    const fileExtname = path.extname(filePath);
 
     // 将待克隆文件相对于目标目录的路径增量部分，作为目标路径的一部分，以保持文件目录结构
-    const relativePath = filePath.substring(sourcePath.length)
-    let targetFilePath = path.join(targetPath, folderName, relativePath)
+    const relativePath = filePath.substring(sourcePath.length);
+    let targetFilePath = path.join(targetPath, folderName, relativePath);
 
     // 若不以文件夹进行分组，则需应用业务名称为文件名
     if (renameFile === true) {
@@ -217,52 +217,52 @@ const clonePath = async (
         targetFilePath,
         path.basename(targetFilePath, fileExtname),
         payload.name
-      )
+      );
     }
-    console.log('targetFilePath: ', targetFilePath)
+    console.log("targetFilePath: ", targetFilePath);
 
     if (payload === null) {
       // 创建镜像
-      console.log('create mirror')
+      console.log("create mirror");
 
       try {
-        fs.copy(filePath, targetFilePath)
+        fs.copy(filePath, targetFilePath);
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
     } else {
       // 创建新文件
-      console.log('create product')
+      console.log("create product");
 
       // 读取当前文件内容为模板
-      let pageContent = await fs.readFile(filePath, 'utf8')
+      let pageContent = await fs.readFile(filePath, "utf8");
 
       // 处理特定后缀名的文件
       const supportedExtnames = [
-        'html',
-        'js',
-        'json',
-        'php',
-        'ts',
-        'text',
-        'vue',
-        'wxml',
-        'wxss'
-      ].map(item => '.' + item)
+        "html",
+        "js",
+        "json",
+        "php",
+        "ts",
+        "text",
+        "vue",
+        "wxml",
+        "wxss",
+      ].map((item) => "." + item);
       if (supportedExtnames.includes(fileExtname)) {
         // 替换模板中的变量标识（[[变量名]]）为实际值
         Object.keys(payload).forEach((name) => {
-          console.log('try mapping ', name)
-          if (!payload[name]) return
+          console.log("try mapping ", name);
+          if (!payload[name]) return;
 
           pageContent = replaceMatchedString(
             pageContent,
             `[[${name}]]`,
             payload[name]
-          )
-        })
+          );
+        });
       } else {
-        console.warn(`${fileExtname} files are not supported now.`)
+        console.warn(`${fileExtname} files are not supported now.`);
       }
 
       // console.log('pageContent: ', pageContent)
@@ -270,37 +270,37 @@ const clonePath = async (
       // 创建并写入文件
       try {
         fs.ensureFile(targetFilePath).then(() => {
-          fs.writeFile(targetFilePath, pageContent)
-        })
+          fs.writeFile(targetFilePath, pageContent);
+        });
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
     }
-  }
+  };
 
   // 迭代处理根目录下的路径
-  await processPath(sourcePath, fileOp)
+  await processPath(sourcePath, fileOp);
 
-  shell.openPath(targetPath) // 在文件管理器中打开目标路径
-}
+  shell.openPath(targetPath); // 在文件管理器中打开目标路径
+};
 
 /** 拷贝文字内容 */
 const copyText = (content) => {
   try {
-    clipboard.writeText(content)
-    console.log('text copied: ', clipboard.readText())
+    clipboard.writeText(content);
+    console.log("text copied: ", clipboard.readText());
   } catch (error) {
-    console.error('copyText error: ', error)
+    console.error("copyText error: ", error);
   }
-}
+};
 
 // 检查配置文件是否存在，若否则迭代创建文件（含路径），一般为初次启动使用
-touchFile(appFile)
+touchFile(appFile);
 
 // 初始化历史记录类
-const historyKeys = ['path', 'url']
-const historyFileName = 'history.json'
-const historyFilePath = path.join(appPathDict.data, historyFileName)
-const historit = new Historit(historyFilePath, historyKeys)
+const historyKeys = ["path", "url"];
+const historyFileName = "history.json";
+const historyFilePath = path.join(appPathDict.data, historyFileName);
+const historit = new Historit(historyFilePath, historyKeys);
 
 /* services/index.js */
